f265fcef4d617eaaed6151bfb3dbc2f8c9e0e38e
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-06-05 17:23:38 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+//1.)  find two different ways to target the first section element on the page. Use console.log to check. Put a comment with the word faster by the method that targets the element more quickly and efficently. 
+
+
+
+//2.)  Target the footer element. Use console.log to check 
+ 
+
+//3.)  Target all elements with the class or orange and change the text in these elements orange 
+
+
+//4.) Target all section elements and console log 
+
+
+
+//5.) Find two ways to target the second section element 
